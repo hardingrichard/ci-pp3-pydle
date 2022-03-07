be46ca414e6be40229b051c285b910c3ec736bab
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-03-07 22:51:14 +0000

    Feat: Add username input prompt and welcome message

--- run.py
+++ run.py
@@ -10,9 +10,19 @@ instead of 6 at guessing the correct word or the game will result in a loss.
 import random
 
 
+def username():
+    """
+    Function to create the player username for greeting
+    """
+    user = input("Please Enter Your Username: ").capitalize()
+    print(f"\nWelcome to Pydle {user}! This is a Python CLI version of the \n"
+          "popular game Wordle. In this version you will have 5 attempts \n"
+          "at guessing the hidden word. To add an extra challenge, the \n"
+          "word you will be guessing is 7 letters long... Good luck!")
+
 
 def main():  # main function placeholder
-    pass
+    username()
 
 
 main()
