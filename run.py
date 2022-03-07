47afa77ed22f50d5aa52959b0975b3285e5b3dad
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-03-07 21:24:04 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Pydle - A python CLI version of the popular Wordle game
+
+The game is a simple word game where the user must find the hidden word by
+making guesses at what is thought the hidden word could be. This version Pydle
+consists of words made up of 7 letters and the user will have 5 attempts
+instead of 6 at guessing the correct word or the game will result in a loss.
+"""
