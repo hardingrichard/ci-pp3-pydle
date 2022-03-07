3b462a0a7b7982a67ecc970f554f8083fd7f0baf
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-03-07 22:01:49 +0000

    Feat: Add main function placeholder and main call ready for executing later on

--- run.py
+++ run.py
@@ -6,3 +6,13 @@ making guesses at what is thought the hidden word could be. This version Pydle
 consists of words made up of 7 letters and the user will have 5 attempts
 instead of 6 at guessing the correct word or the game will result in a loss.
 """
+
+import random
+
+
+
+def main():  # main function placeholder
+    pass
+
+
+main()
