df506220ddc90748e37180c8e2a1ebc1cff3f0b4
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-03-09 19:50:35 +0000

    Add pydle_logic.py to seperate the logic functions from the interface to be imported in run.py

---
+++ pydle_logic.py
@@ -0,0 +1,58 @@
+"""
+pydle_logic.py contains the functions used for the logic of the Pydle game.
+Seperate from main run.py for ease of reference within the game code and
+to make it easier to read.
+"""
+
+from character_rule import CharacterRule
+
+
+class Pydle:
+    # Class constants
+    WORD_SIZE = 7
+    GUESS_MAX = 5
+
+    def __init__(self, hidden: str):  # set argument 'hidden' to string
+        self.hidden: str = hidden
+        self.guesses = []
+
+    def guess(self, word: str):
+        self.guesses.append(word)
+
+    def guess_attempt(self, word: str):
+        """
+        Compares the user guess against hidden word and gives feedback for the
+        letter by looping through the hidden word letter by letter and checking
+        if the character is valid.
+        """
+        guess_result = []  # list of character rules
+
+        for i in range(self.WORD_SIZE):
+            character_x = word[i]
+            letter = CharacterRule(character_x)
+            letter.correct_letter = character_x in self.hidden
+            letter.correct_position = character_x == self.hidden[i]
+            guess_result.append(letter)
+
+        return guess_result
+
+    @property
+    def correct_guess(self):
+        """
+        True if current guess is equal to the hidden word then win condition
+        otherwise False
+        """
+        return len(self.guesses) > 0 and self.guesses[-1] == self.hidden
+
+    @property
+    def guess_remain(self) -> int:  # return as an integer
+        return self.GUESS_MAX - len(self.guesses)
+
+    @property
+    def guess_still(self):
+        """
+        When user has used all their guesses or correctly guessed then can no
+        longer continue guessing. True when current number of guesses is less
+        than the max guess of 5
+        """
+        return self.guess_remain > 0 and not self.correct_guess
