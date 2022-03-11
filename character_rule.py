888feeede6f91b298befd5bcd0a062cfc689d9db
Richard J Harding <richard.jharding@hotmail.co.uk>
2022-03-11 23:13:07 +0000

    Refactor: Amend docstrings and make spelling and grammar changes

--- character_rule.py
+++ character_rule.py
@@ -1,14 +1,14 @@
 """
 character_rule.py file holds the class CharacterRule which is imported into
 the main run.py file and referred to within functions. CharacterRule was
-seperated into its own file to tidy up code and to easily refer to.
+separated into a further file to tidy up code and to easily refer to.
 """
 
 
 class CharacterRule:
     """
-    Holds the rules for if the letters are in the hidden word or in the
-    correct positon.
+    Holds the rules for if the letters are in the hidden word or the
+    correct position.
     """
     def __init__(self, word_letter: str):  # set 'word_letter' to string
         self.word_letter: str = word_letter
@@ -17,8 +17,8 @@ class CharacterRule:
 
     def __repr__(self):
         """
-        function overrides the information displayed in terminal for
-        CharacterRule object. Making it easier to read feedback from user
+        Function overrides the information displayed in the terminal for the
+        CharacterRule object. Making it easier to read feedback from the user
         guess with the correct letter and correct position when debugging.
         """
         return (
